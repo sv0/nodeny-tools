ec59f573b3e7b2c1c1c40aa46127a7c67d4a7fac
Viacheslav Svyrydiuk <svyrydiuk@gmail.com>
2013-03-18 19:04:59 +0200

    nodeny web auth shell script

---
+++ noauth.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+HOST=$1
+USERNAME=$2
+PASSWORD=$3
+
+SCHEMA="http"
+URI="/cgi-bin/stat.pl"
+URL="$SCHEMA://$HOST$URI"
+AUTH_URL="$URL?uu=$USERNAME&a=98" 
+
+get_ses()
+{
+    echo `wget -q -O- $URL | grep -oE -e "([[:digit:]]{6})"`
+}
+
+md5hash()
+{
+    # $1 - ses (session key)
+    # $2 - PASSWORD
+    echo -n "$1 $2" | md5sum - | awk '{print $1}'
+}
+
+if [[ $1 == '-h' || $1 == '--help' ]];
+then
+    echo "Nodeny web authentication."
+    echo "Usage:" `basename $0` "HOST USERNAME PASSWORD"
+    echo ""
+fi
+
+SES=`get_ses`;
+PP=`md5hash $SES $PASSWORD`
+
+while true;
+do
+    auth_result=`wget -q -O- "$URL?uu=$USERNAME&a=98&pp=$PP"`;
+    echo $auth_result | grep -q 'meta http-equiv="refresh"' 
+    if [ 0 -eq $? ];
+    then
+        # AUTH OK
+        sleep 40;
+    else
+        # it seems that ses key was expired
+        # get new ses and pp
+        SES=`get_ses`;
+        PP=`md5hash $SES $PASSWORD`
+    fi;
+done;
+
